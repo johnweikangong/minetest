ce08f4a3d02360e08a9f827386cb460453fc03bb
johnweikangong <ongwk95@hotmail.com>
2017-11-07 21:59:12 +0800

    Add powerdrill that is capable of mining any type of block with a long range

--- init.lua
+++ init.lua
@@ -1,16 +1,18 @@
 minetest.register_tool("features:powerdrill", {
     description = "Powerful drill that destroys everything",
     inventory_image = "features_powerdrill.png",
-	liquids_pointable = true,
-		range = 14,
-		tool_capabilities = {
-			full_punch_interval = 0,
-			max_drop_level=3,
-			groupcaps={
-			times = {0, 0, 0},
-			uses = 0,
-			maxlevel = 3
+	range = 20,
+	tool_capabilities = {
+		max_drop_level = 3,
+		groupcaps = { -- Ensures that any type of block will be mined instantly
+			unbreakable =   {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			dig_immediate = {times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			fleshy =	{times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			choppy =	{times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			bendy =		{times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			cracky =	{times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			crumbly =	{times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3},
+			snappy =	{times={[1] = 0, [2] = 0, [3] = 0}, uses = 0, maxlevel = 3}
 		},
-			damage_groups = {fleshy = 20},
-		},
+	}
 })
